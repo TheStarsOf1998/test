f56d21556ac6d52e807e44920ff0ac74b04059d8
TheStarsOf1998 <thecatsof520@gmail.com>
2018-11-27 20:41:50 +0800

    http commit

---
+++ repository/demo/Hello.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Hello {
+    public static void show() {
+        System.out.println("Hello World!");
+    }
+
+    public void test() {
+        System.err.println("test");
+    }
+}
---
+++ repository/demo/Test.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Test {
+    public static void main(String[] args) {
+        // Hello.show();
+        // System.out.println("赵俊");
+        // demo.Hello.show();
+        Hello h = new Hello();
+        h.test();
+    }
+}
